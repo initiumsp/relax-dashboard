c975777d917cd9539ce4418fd1405f8338cfd548
joetsuihk <joetsuihk@gmail.com>
2016-06-24 19:12:16 +0800

    added live url as comment

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?php
 
 $base_url = 'http://dev.documentonready.com/brexit_map/';
+// $base_url = 'http://project.initiumlab.com/relax-dashboard/';
 if(!isset($tc_title)) $tc_title = '12格遇上天藍';
 if(!isset($sc_title)) $sc_title = '12格遇上天藍';
 if(!isset($tc_desc)) $tc_desc = '你最近還好嗎﹖尚愛看少女／少男漫畫嗎﹖';
