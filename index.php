8b64149df2398a0198e24a51540e86b618044e88
matthew@documentonready.com <matthew@williams-Mac-mini.local>
2016-07-08 12:04:12 +0800

    [#38] browser tab title remove 端傳媒 Initium Media

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <meta charset="UTF-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title><?php echo $tc_title;?> | 端傳媒 Initium Media</title>
+<title><?php echo $tc_title;?></title>
 <link rel="shortcut icon" href="https://d3rhr7kgmtrq1v.cloudfront.net/static/img/favicon.ico">
 <meta name="description" content="<?php echo $tc_desc;?>">
 <meta name="viewport" content="width=device-width,initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
